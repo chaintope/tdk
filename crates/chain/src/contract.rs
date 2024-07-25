327230a53a98d560a0397f2c8f1edfc35d9145c0
Yamaguchi <h_yamaguchi@chaintope.com>
2024-07-25 14:29:17 +0900

    Fix docs

--- crates/chain/src/contract.rs
+++ crates/chain/src/contract.rs
@@ -1,8 +1,13 @@
-use alloc::{boxed::Box, collections::BTreeMap, string::String, vec::Vec};
+//! Module for Pay-To-Contract
+//!
+//! This module contains an implementation for storing Contract information and using it in a wallet.
+use alloc::{string::String, vec::Vec};
 use tapyrus::PublicKey;
 
+/// The [`ChangeSet`] represents changes to [`Contract`].
 pub type ChangeSet = Vec<Contract>;
 
+/// Contract is a data structure for holding information for receiving payments to pay-to-contract.
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(
     feature = "serde",
@@ -10,8 +15,12 @@ pub type ChangeSet = Vec<Contract>;
     serde(crate = "serde_crate",)
 )]
 pub struct Contract {
+    /// The contract ID is an identifier that uniquely identifies the contract.
     pub contract_id: String,
+    /// The contract represents the content of the contract.
     pub contract: Vec<u8>,
+    /// Public key for generating P2C addresses.
     pub payment_base: PublicKey,
+    /// Set to 1 if available for payment, 0 if not
     pub spendable: bool,
 }
