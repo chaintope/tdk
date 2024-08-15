fef05afb5e59873975e8ec26de803776cf1e94e8
Yamaguchi <h_yamaguchi@chaintope.com>
2024-08-16 02:05:41 +0900

    Fix signing for pay to contract transaction

--- crates/chain/src/contract.rs
+++ crates/chain/src/contract.rs
@@ -5,8 +5,8 @@ use alloc::collections::BTreeMap;
 
 use alloc::{string::String, vec::Vec};
 use num_bigint::BigUint;
-use tapyrus::key::Error;
-use tapyrus::secp256k1::{Scalar, SecretKey};
+use tapyrus::key::{Error, Secp256k1};
+use tapyrus::secp256k1::{All, Scalar};
 use tapyrus::{
     hashes::{Hash, HashEngine},
     PrivateKey,
@@ -38,13 +38,14 @@ impl Contract {
     /// Create private key for Pay-to-Contract
     pub fn create_private_key(
         &self,
+        payment_base_private_key: &PrivateKey,
         payment_base: &PublicKey,
         network: Network,
     ) -> Result<PrivateKey, Error> {
         let commitment: Scalar =
             Self::create_pay_to_contract_commitment(payment_base, self.contract.clone());
-        let sk = SecretKey::from_slice(&commitment.to_be_bytes())?;
-        Ok(PrivateKey::new(sk, network))
+        let p2c_private_key = payment_base_private_key.inner.add_tweak(&commitment)?;
+        Ok(PrivateKey::new(p2c_private_key, network))
     }
 
     /// Compute pay-to-contract commitment as Scalar.
@@ -68,5 +69,65 @@ impl Contract {
         let mut value = [0u8; 32];
         value[32 - bytes.len()..].copy_from_slice(&bytes);
         Scalar::from_be_bytes(value).unwrap()
+    }
+
+    /// Generate public key for Pay-to-Contract
+    pub fn create_pay_to_contract_public_key(
+        payment_base: &PublicKey,
+        contracts: Vec<u8>,
+        secp: &Secp256k1<All>,
+    ) -> PublicKey {
+        let commitment: Scalar =
+            Self::create_pay_to_contract_commitment(payment_base, contracts.clone());
+        let pubkey = payment_base.inner.add_exp_tweak(secp, &commitment).unwrap();
+        PublicKey {
+            compressed: true,
+            inner: pubkey,
+        }
+    }
+}
+
+#[cfg(test)]
+mod signers_container_tests {
+    use core::str::FromStr;
+    use std::string::ToString;
+
+    use tapyrus::key::Secp256k1;
+
+    use super::*;
+    use crate::tapyrus::hashes::hex::FromHex;
+
+    #[test]
+    fn test_create_private_key() {
+        let payment_base_private_key = PrivateKey::from_slice(
+            &Vec::<u8>::from_hex(
+                "c5580f6c26f83fb513dd5e0d1b03c36be26fcefa139b1720a7ca7c0dedd439c2",
+            )
+            .unwrap(),
+            Network::Dev,
+        )
+        .unwrap();
+        let payment_base =
+            PublicKey::from_private_key(&Secp256k1::signing_only(), &payment_base_private_key);
+        let contract = Contract {
+            contract_id: "contract_id".to_string(),
+            contract: "metadata".as_bytes().to_vec(),
+            payment_base,
+            spendable: true,
+        };
+        let key =
+            contract.create_private_key(&payment_base_private_key, &payment_base, Network::Dev);
+        assert!(key.is_ok());
+        assert_eq!(
+            key.unwrap(),
+            PrivateKey::from_slice(
+                &Vec::<u8>::from_hex(
+                    "78612a8498322787104379330ec41f749fd2ada016e0c0a6c2b233ed13fc8978"
+                )
+                .unwrap(),
+                Network::Dev
+            )
+            .unwrap()
+        );
     }
 }
